4ba9a505c206df17f031c4c3c6035696434e3287
Jeff Pickell <pickellj@wwt.com>
2015-11-06 08:40:26 -0600

    perl version of git-branch

---
+++ bin/git-branch.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# [git@prodgithub01.wwt.com:pickellj/dots.git - refs/heads/master]
+
+if ( ! -e "/usr/bin/git" ){ die "$!" }
+
+chomp($branch=`git symbolic-ref HEAD|cut -f3 -d'/'`);
+@repo=split(/[@\.:]/, `git config --get remote.origin.url`);
+
+if ($repo[0] == "git"){
+	$remote = $repo[1];
+}
+
+if ($repo[0] == "https"){
+	$remote = $repo[1];
+}
+
+print ("\n[$Frame$remote - $branch]\n");
+
