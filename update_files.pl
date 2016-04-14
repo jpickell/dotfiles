2480e170f576a756a4d13c9ce4a330f676b7aefc
Jeff <jkp@helixbits.com>
2016-04-14 10:54:50 -0500

    Initial skeleton for updating local files - needs more testing

---
+++ update_files.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+# This script will pull all of the active files into the repo to be synced
+#
+
+@files = `ls -a`;
+
+foreach $f(@files){
+	chomp $f;
+	if(($f =~ /^\.[a-z]/)&&(! -l $f)&&($f ne ".git")){
+		# need to make sure that the file isn't a symlink (.vimrc)
+		if(! -l $f){
+		print("copying $f to ~/$f\n");
+		#system("cp -pr $f ~/$f");
+		print("cp -pr $f ~/$f\n");
+		}
+	}
+}
